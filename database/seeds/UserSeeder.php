200cfb7488842b1233f67707f6165d7759a18792
348858954@qq.com <348858954@qq.com>
2019-09-02 14:55:52 +0800

    zzy

---
+++ database/seeds/UserSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $users = factory(\App\User::class,10)->create();
+        $user = $users[0];
+        $user->name = 'zzy';
+        $user->email = '348858954@qq.com';
+        $user->save();
+    }
+}
