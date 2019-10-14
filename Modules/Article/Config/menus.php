59891a6d14a87785a6575e96b8875a8725951817
echozzy <348858954@qq.com>
2019-10-14 15:01:03 +0800

    menus

--- Modules/Article/Config/menus.php
+++ Modules/Article/Config/menus.php
@@ -1,42 +1,29 @@
 <?php
 /** .-------------------------------------------------------------------
- * |      Site: www.hdcms.com  www.houdunren.com
- * |      Date: 2018/7/2 上午12:54
- * |    Author: 向军大叔 <2300071698@qq.com>
+ * |      Site: www.zhouzy365.com
+ * |      Date: 2019/9/10 下午3:13
+ * |    Author: zzy <348858954@qq.com>
  * '-------------------------------------------------------------------*/
 /**
  * 后台菜单配置
  * 下面是属性说明：
  * title 菜单栏目
- * icon 图标参考 http://fontawesome.dashgame.com/
- * menus 子菜单
+ * p_id 父级菜单
+ * icon 图标参考 https://fontawesome.com 或 http://www.fontawesome.com.cn
  * permission 权限标识，必须在permission.php配置文件中存在
+ * url 菜单地址
+ * menus 子菜单
  */
 return [
     [
-        "title"      => "系统管理",
+        "title"      => "文章管理",
         "p_id"        => 0,//父级ID
-        "icon"       => "fa fa-cogs",
-        'permission' => 'AdminController',
-        "url" => "链接地址",
+        "icon"       => "fas fa-file-alt",
+        'permission' => 'Article',
+        "url" => "/article/admin_article",
         "menus"      => [
-            ["title" => "网站信息", "icon"=> "fa fa-navicon","permission" => "AdminController@config", "url" => "链接地址"],
-            ["title" => "邮箱配置", "icon"=> "fa fa-navicon","permission" => "AdminController@email", "url" => "链接地址"],
-            ["title" => "友情链接", "icon"=> "fa fa-navicon","permission" => "AdminController@link", "url" => "链接地址"],
-            ["title" => "后台菜单", "icon"=> "fa fa-navicon","permission" => "Modules\Admin\Http\Controllers\MenuController@index", "url" => "/admin/menu"],
-        ],
-    ],
-    [
-        "title"      => "权限管理",
-        "p_id"        => 0,//父级ID
-        "icon"       => "fas fa-user-shield",
-        'permission' => 'Permission',
-        "url" => "链接地址",
-        "menus"      => [
-            ["title" => "管理员列表", "icon"=> "fa fa-navicon", "permission" => "Modules\Admin\Http\Controllers\AdminUserController@index", "url" => "链接地址"],
-            ["title" => "管理员日志", "icon"=> "fa fa-navicon", "permission" => "PermissionController@log", "url" => "链接地址"],
-            ["title" => "角色管理", "icon"=> "fa fa-navicon", "permission" => "Modules\Admin\Http\Controllers\RoleController@index", "url" => "/admin/role"],
-            ["title" => "权限列表", "icon"=> "fa fa-navicon", "permission" => "Modules\Admin\Http\Controllers\PermissionController@index", "url" => "/admin/permission"],
+            ["title" => "文章分类", "icon"=> "fa fa-navicon", "permission" => "Modules\Article\Http\Controllers\AdminCategoryController@index", "url" => "/article/admin_article"],
+            ["title" => "文章列表", "icon"=> "fa fa-navicon", "permission" => "Modules\Article\Http\Controllers\AdminArticleController@index", "url" => "/article/admin_article"],
         ],
     ],
 ];
